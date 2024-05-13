127d20f71b6c427d82663931bdd00fa9df2f0f0f
Vuori1 <78846315+Vuori1@users.noreply.github.com>
2024-05-13 20:31:28 +0300

    lisätty vihollisia ja vihollis spawneja, enemy "animaatio"

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,28 +2,38 @@ extends CharacterBody2D
 
 
 @export var movement_speed=40.0
-@export var hp=4
+@export var hp=20
 @export var knockback_recovery = 3.5
 @export var experience = 1
+@export var damage = 1
 var knockback = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var snd_hit = $snd_hit
 @onready var snd_death = $snd_death
 @onready var loot_base = get_tree().get_first_node_in_group("loot")
+@onready var hitBox = $HitBox
+@onready var sprite = $Sprite2D
 
 var death_anim = preload("res://Enemy/explosion.tscn")
 var exp_gem = preload("res://Objects/experience_gem.tscn")
-
 signal remove_from_array(object)
 
+func _ready():
+	hitBox.damage = damage
+	
+	
 func  _physics_process(_delta):
-	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction*movement_speed
-	velocity += knockback
 	move_and_slide()
-
+	if direction.x > 0.1:
+		sprite.flip_h = true;
+	elif direction.x < -0.1:
+		sprite.flip_h = false;
+	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
+	
+	velocity = direction*movement_speed
+	velocity += knockback		
 func death():
 		emit_signal("remove_from_array",self)
 		var enemy_death = death_anim.instantiate()
