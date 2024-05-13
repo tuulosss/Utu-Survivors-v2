edf2f5717df2d47b00e21e9f4e045ff9130887f0
tuulosss <samu@tuulos.net>
2024-05-13 23:46:43 +0300

    ALPHA VERSION 1.0 RELEASE
    ALPHA DEMO BETA CHARLIE VERSION 1.0 RELEASE

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -38,7 +38,7 @@ func  _physics_process(_delta):
 func death():
 		emit_signal("remove_from_array",self)
 		var enemy_death = death_anim.instantiate()
-		enemy_death.scale = $Sprite2D.scale
+		enemy_death.scale = $Sprite2D.scale*10
 		enemy_death.global_position = global_position
 		get_parent().call_deferred("add_child",enemy_death)
 		var new_gem = exp_gem.instantiate()
